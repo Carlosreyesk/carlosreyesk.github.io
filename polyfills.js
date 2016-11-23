c2caf5479fc3dae53db4bd1e2e9d8b208c8935eb
carlosreyesk <carlosreyesk@gmail.com>
2016-11-23 11:00:20 -0600

    url change

--- polyfills.js
+++ polyfills.js
@@ -89,7 +89,7 @@
 /******/ 	__webpack_require__.c = installedModules;
 
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "http://localhost:8080/";
+/******/ 	__webpack_require__.p = "https://carlosreyesk.github.io/";
 /******/ })
 /************************************************************************/
 /******/ ([]);
