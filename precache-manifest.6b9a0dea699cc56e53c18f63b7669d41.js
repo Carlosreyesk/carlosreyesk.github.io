bf6e145d0d30718300f6d4eb051cf0f260028c05
Carlos Reyes <carlos@zarmada.com>
2020-04-07 18:52:49 -0600

    Updates

---
+++ precache-manifest.6b9a0dea699cc56e53c18f63b7669d41.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "951d1c892f399832e5aae992fcab2cd3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a4cf595a212c9e46a0b",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "41bd30bd5affef7fbe41",
+    "url": "/static/js/2.8bc0949e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8bc0949e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a4cf595a212c9e46a0b",
+    "url": "/static/js/main.b0ac6ba6.chunk.js"
+  },
+  {
+    "revision": "6a3ba521a668b4faa410",
+    "url": "/static/js/runtime-main.248e62a3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
